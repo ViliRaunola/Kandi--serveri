f8b725fbcefc840067b5f7f92072e218ffdc9c35
Vili Raunola <raunolavili@gmail.com>
2022-03-18 09:38:37 +0200

    can connect to live mongodb

--- config/database.js
+++ config/database.js
@@ -1,3 +1,4 @@
 module.exports = {
-    database: 'mongodb://localhost:27017/kandiserveri',
+    //database: 'mongodb://localhost:27017/kandiserveri',
+    database: 'mongodb+srv://admin:U3DIIAEpZ5Ffu0Qk@meanauthapp.n22zp.mongodb.net/meanauthapp?retryWrites=true&w=majority'
 }
