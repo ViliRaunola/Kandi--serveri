179fe8fdc6062f742f03dc30020b669a957dc15b
Vili Raunola <raunolavili@gmail.com>
2022-03-20 08:48:18 +0200

    first commit

--- routes/api.js
+++ routes/api.js
@@ -4,42 +4,87 @@ const Wifi = require('../models/Wifi')
 const Bluetooth = require('../models/Bluetooth')
 
 //Route for saving the data 
+// router.post('/save/wifi', (req, res, next) => {
+//     const options = {upsert: true};
+
+//     //console.log(req.body)
+
+//     //Saving the wifi data   
+//     wifi_list = req.body.wifi;
+//     wifi_list.forEach(data => {
+//         Wifi.findOneAndUpdate({
+//         MAC_Address: data.MAC_Address}, 
+//         {
+//         First_Seen: data.First_Seen, 
+//         Last_Seen: data.Last_Seen,
+//         Signal_Strength: data.Signal_Strength,
+//         ESSID: data.ESSID,
+//         BSSID: data.BSSID,
+//         Probed_ESSID: data.Probed_ESSID,
+//         Is_AP: data.Is_AP
+//         }, 
+//         options, (err) => {
+//             if(err) {
+//                 console.log(err)
+//                 res.json({success: false});
+//                 return err;
+//             }
+//         });
+//     });
+//     console.log('Saved data to databse')
+//     res.json({success: true})
+// });
+
 router.post('/save/wifi', (req, res, next) => {
-    const options = {upsert: true};
 
-    //console.log(req.body)
-
-    //Saving the wifi data   
-    wifi_list = req.body.wifi;
+    //Saving the wifi data
+    var wifi_list = req.body.wifi;
     wifi_list.forEach(data => {
-        Wifi.findOneAndUpdate({
-        MAC_Address: data.MAC_Address}, 
-        {
-        First_Seen: data.First_Seen, 
-        Last_Seen: data.Last_Seen,
-        Signal_Strength: data.Signal_Strength,
-        ESSID: data.ESSID,
-        BSSID: data.BSSID,
-        Probed_ESSID: data.Probed_ESSID,
-        Is_AP: data.Is_AP
-        }, 
-        options, (err) => {
-            if(err) {
-                console.log(err)
+        Wifi.findOne({MAC_Address: data.MAC_Address} ,(err, found) => {
+            if(err){
                 res.json({success: false});
+                console.log(err)
                 return err;
             }
+            if(found){
+                found.Last_Seen = data.Last_Seen;
+                found.Signal_Strength = data.Signal_Strength;
+                found.save(err => {
+                    if(err){
+                        res.json({success: false});
+                        console.log(err)
+                        return err;
+                    }  
+                })
+            }else{
+                Wifi.create({
+                    First_Seen: data.First_Seen, 
+                    Last_Seen: data.Last_Seen,
+                    Signal_Strength: data.Signal_Strength,
+                    ESSID: data.ESSID,
+                    BSSID: data.BSSID,
+                    Probed_ESSID: data.Probed_ESSID,
+                    Is_AP: data.Is_AP
+                },
+                (err) => {
+                    if(err){
+                        res.json({success: false});
+                        console.log(err)
+                        return err;
+                    }
+                });
+            }
         });
-    });
+    })
     console.log('Saved data to databse')
-    res.json({success: true})
+    return res.json({success: true})
 });
 
+
 router.post('/save/bt', (req, res, next) => {
-    const options = {upsert: true};
 
     //Saving the bluetooth data
-    bluetooth_list = req.body.bluetooth;
+    var bluetooth_list = req.body.bluetooth;
     bluetooth_list.forEach(data => {
         Bluetooth.findOne({MAC_Address: data.MAC_Address} ,(err, found) => {
             if(err){
@@ -77,7 +122,7 @@ router.post('/save/bt', (req, res, next) => {
         });
     })
     console.log('Saved data to databse')
-    res.json({success: true})
+    return res.json({success: true})
 });
 
 router.get('/data', (req, res, next) => {
